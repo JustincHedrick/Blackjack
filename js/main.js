52307f92cdb3c0efc631ce1f9521bfe2d78f8b33
Justin Hedrick <Justinchedrick@gmail.com>
2022-04-04 16:41:37 -0700

    added functionality with DOM, populated cards in arrays and disabled buttons during runtime

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
 const players = [
-  {'1': 'player', wager:0, score: 0},
-  {'-1': 'dealer', score: 0}
+  {'1': 'player'},
+  {'-1': 'dealer'}
 ];
 const cardSuits = ['s', 'c', 'd', 'h'];
 const cardRank = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
@@ -13,17 +13,23 @@ const betAmounts = {
 };
 /*----- app's state (variables) -----*/
 let gameStatus;
-let playerHand = [];
+let playerHand = {};
 let dealerHand = [];
 let aceCount;
 let shuffledDeck;
+let totalWager = 0;
+let bankTotal = 1000;
+let playerScore = 0;
+let dealerScore = 0;
 /*----- cached element references -----*/
 let dealButtonEl = document.getElementById('deal-button');
+let hitStayEl = document.getElementById('hit-stay');
 let hitButtonEl = document.getElementById('hit-button');
 let stayButtonEl = document.getElementById('stay-button');
 let chipEl = document.getElementById('coin-container'); 
 let wagerEl = document.getElementById('wager-total');
-let bankEl = document.getElementById('bank-total > p');
+let totalWagerEl = document.getElementById('total-wager');
+let bankEl = document.getElementById('bank');
 let playerHandEl = document.getElementById('player');
 let dealerHandEl = document.getElementById('dealer');
 let playerTotalEl = document.getElementById('total-player');
@@ -51,40 +57,52 @@ function buildDeck() {
       deck.push({
         face: `${suit}${rank}`,
         value: Number(rank) || (rank === 'A' ? 11 : 10)
-      })
-    })
-  }) 
+      });
+    });
+  }); 
+  return deck;
 }
 
 function newShuffle() {
   const tempDeck = [...masterDeck];
-  const newShuffledDeck = [];
+  shuffledDeck = [];
   while (tempDeck.length) {
     const rndIdx = Math.floor(Math.random() * tempDeck.length);
-    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
   };
+  return shuffledDeck;
 };
 
 function getWager(evt) {
-  console.log(evt.target);
+  if (evt.target.id === 'ten') totalWager += betAmounts.ten;
+  if (evt.target.id === 'twenty-five') totalWager += betAmounts.twenty;
+  if (evt.target.id === 'hundred') totalWager += betAmounts.hundred;
+  if (totalWager > 0) dealButtonEl.style.visibility = 'visible';
+  totalWagerEl.innerHTML = `${totalWager}`;
+  return totalWager;
 };
 
 function dealCards(evt) {
-  console.log(evt.target);
-}
+  if (evt.target.id === 'deal-button') {
+    bankEl.innerHTML = bankTotal - totalWager;
+    hitStayEl.style.visibility = 'visible';
+    chipEl.style.pointerEvents = 'none';
+  };
+  playerHand = [shuffledDeck.shift(), shuffledDeck.shift()];
+  dealerHand = [shuffledDeck.shift(), shuffledDeck.shift()];
+};
+
 
 function playerHit(evt) {
-  console.log(evt.target);
-}
+  playerScore = 0;
+  if (evt.target.id === 'hit-btn') {
+    playerHand.push(shuffledDeck.shift());
+  };
+};
 
 function playerStay(evt) {
-  console.log(evt.target);
-}
-
-function compareValue() {
 
 };
 
 function render() {
-
-}
+};
